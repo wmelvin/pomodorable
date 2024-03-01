12cbf402fcf6bff41e28f8744c1d4c8f4c25950b
Bill Melvin <bill@billmelvin.com>
2024-03-01 12:00:51 -0500

    Initial countdown and time display implementation

--- src/pomodorable/ui.py
+++ src/pomodorable/ui.py
@@ -1,16 +1,49 @@
+from datetime import datetime
+
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal
+from textual.reactive import reactive
 from textual.widgets import Button, Footer, Header, Input, Label, Log, Static
 
 APP_NAME = "Pomodorable"
 
+DEFAULT_SESSION_SECONDS = 25 * 60  # TODO: settings.default_session_minutes
 
 class CountdownDisplay(Static):
-    """00:00 (minutes:seconds)"""
+    """[00:]00:00 ([hours:]minutes:seconds)"""
+
+    seconds = reactive(DEFAULT_SESSION_SECONDS)
+
+    def add_minutes(self, minutes: int) -> None:
+        self.seconds += minutes * 60
+        if self.seconds < 0:
+            self.seconds = 0
+
+    def reset_countdown(self) -> None:
+        self.seconds = DEFAULT_SESSION_SECONDS
+
+    def watch_seconds(self, seconds: datetime) -> None:
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        if hours > 0:
+            self.update(f"{hours:02}:{minutes:02}:{seconds:02}")
+        else:
+            self.update(f"{minutes:02}:{seconds:02}")
 
 
 class TimeDisplay(Static):
     """00:00:00 (hours:minutes:seconds)"""
+
+    time = reactive(datetime.now())
+
+    def on_mount(self) -> None:
+        self.set_interval(1 / 5, self.update_time)
+
+    def update_time(self) -> None:
+        self.time = datetime.now()
+
+    def watch_time(self, time: datetime) -> None:
+        self.update(f"{time.strftime('%H:%M:%S')}")
 
 
 class PomodoroApp(App):
@@ -57,6 +90,12 @@ class PomodoroApp(App):
         btn = event.button.id
         log = self.query_one(Log)
         log.write_line(f"You pressed [{btn}].")
+        if btn == "btn-plus-five":
+            disp = self.query_one(CountdownDisplay)
+            disp.add_minutes(5)
+        if btn == "btn-minus-five":
+            disp = self.query_one(CountdownDisplay)
+            disp.add_minutes(-5)
 
     def action_exit_app(self) -> None:
         self.exit()
