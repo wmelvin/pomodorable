021d82514838c267e6d02a23af7bad45e1b338e7
Bill Melvin <bill@billmelvin.com>
2024-03-23 18:41:37 -0400

    Add max length check to MRUList test

--- tests/test_mru_list.py
+++ tests/test_mru_list.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pomodorable.mru_list import MRUList
+from pomodorable.mru_list import MRU_LIST_MAX, MRUList
 
 
 def test_mru_list(tmp_path: Path):
@@ -22,3 +22,18 @@ def test_mru_list(tmp_path: Path):
     mru_list2.load()
     assert mru_list2.get_tasks() == ["task3", "task2", "task1"]
     assert mru_list2.get_reasons() == ["reason1", "reason3", "reason2"]
+
+    for i in range(MRU_LIST_MAX + 4):
+        mru_list2.add_task(f"task{i}")
+        mru_list2.add_reason(f"reason{i}")
+    mru_list2.save()
+
+    mru_list3 = MRUList(tmp_path)
+    mru_list3.load()
+    tasks = mru_list3.get_tasks()
+    assert len(tasks) == MRU_LIST_MAX
+    reasons = mru_list3.get_reasons()
+    assert len(reasons) == MRU_LIST_MAX
+    #  If MRU_LIST_MAX is changed, these will need to be updated.
+    assert tasks[0] == "task15"
+    assert reasons[0] == "reason15"
