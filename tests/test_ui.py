09c01fc3508dd727c58fbfb3fd3ed36885b35f69
Bill Melvin <bill@billmelvin.com>
2024-07-01 13:52:18 -0400

    Increase pause delay in test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -56,7 +56,7 @@ async def test_app_plays_wav_file_at_finish(tmp_path, wav_name, expect_text):
 async def test_minus_button_stops_at_one(tmp_path):
     app_data = AppData(init_data_path=tmp_path)
     app = PomodorableApp(init_app_data=app_data)
-    pause_secs = 0.1  # None
+    pause_secs = 0.2  # None
     async with app.run_test() as pilot:
         countdown = pilot.app.query_one("#countdown")
         assert countdown.seconds == 25 * 60  # Should be 25 minutes by default.
